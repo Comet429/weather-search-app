5caa94417af30763a5d673b0ec06be183c7738a9
Jessie P. <ritime275@gmail.com>
2024-10-13 02:56:00 -0400

    Added footer and links

--- src/App.js
+++ src/App.js
@@ -8,9 +8,35 @@ function App() {
         <h1>Weather App</h1>
         <SearchEngine />
       </header>
-      <footer>
-        <a href="">Open sourced code</a>, by <a herf="">Jessie Polong</a> for{" "}
-        <a herf="">SheCodes</a>
+      <footer className="App-footer">
+        <p>
+          This project was coded by
+          <a
+            href="https://www.shecodes.io/graduates/127780-jessie-polong"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Shecodes
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/Comet429/weather-search-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>{" "}
+          for{" "}
+          <a
+            href="https://app.netlify.com/sites/spontaneous-dragon-099add/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>{" "}
+        </p>
       </footer>
     </div>
   );
